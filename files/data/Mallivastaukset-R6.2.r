6b9442415e43d934b46dce815b99c7e3d2e7a62b
hpsaarimaki <57613974+hpsaarimaki@users.noreply.github.com>
2022-10-12 15:11:15 +0300

    Rename Mallivastaukset R6.2 Faktorianalyysi.r to Mallivastaukset-R6.2.r

--- files/data/Mallivastaukset-R6.2.r
+++ files/data/Mallivastaukset-R6.2.r
@@ -1,7 +1,7 @@
 # PSY204 syksy 2022
 # Harjoitusmoniste 6.2: Faktorianalyysi
 # Mallivastaukset
-# Heini Saarim‰ki 9.10.2022
+# Heini Saarim√§ki 9.10.2022
 # 
 # ----
 
@@ -9,7 +9,7 @@
 library(psych)
 library(GPArotation)
 
-# Asetetaan tyˆskentelykansio
+# Asetetaan ty√∂skentelykansio
 setwd("C:/Users/sbhesa/Documents/Opetus/")
 
 # ----
@@ -44,11 +44,11 @@ ggcorr(tas)
 
 # Kaksi keinoa:
 
-# 1) Bartlettin sf‰‰risyystesti
+# 1) Bartlettin sf√§√§risyystesti
 
 cortest.bartlett(tas)
 
-# Merkitsev‰ tulos (p<.05), eli ainakin jotkin muuttujat korreloivat kesken‰‰n.
+# Merkitsev√§ tulos (p<.05), eli ainakin jotkin muuttujat korreloivat kesken√§√§n.
 
 # 2) KMO
 
@@ -60,9 +60,9 @@ KMO(tas)
 
 # 2. Faktorimallin suunnittelu
 
-# Faktorien m‰‰r‰n valinta
+# Faktorien m√§√§r√§n valinta
 
-# Kaksi menetelm‰‰:
+# Kaksi menetelm√§√§:
 
 # 1) Scree-kuvio
 
@@ -74,13 +74,13 @@ scree(tas)
 
 fa.parallel(tas)
 
-# arviolta 4 faktoria, joten aloitetaan siit‰
+# arviolta 4 faktoria, joten aloitetaan siit√§
 
 # ---
 
 # 3. Faktorianalyysin toteutus
 
-# Kokeillaan nelj‰n faktorin ratkaisua:
+# Kokeillaan nelj√§n faktorin ratkaisua:
 
 fa4 <- fa(tas,
           nfactor=4,
@@ -91,7 +91,7 @@ fa4 <- fa(tas,
 fa.diagram(fa4)
 fa4
 
-# Jatketaan nelj‰n faktorin mallin tarkasteluilla.
+# Jatketaan nelj√§n faktorin mallin tarkasteluilla.
 
 # ---
 
@@ -103,14 +103,14 @@ fa4
 
 # Faktoriratkaisun sopivuus:
 # khii2(116)=589.6, p<.0001
-# TLI = 0.891 --> alle 0.9 olisi ok, eli ei hyv‰
+# TLI = 0.891 --> alle 0.9 olisi ok, eli ei hyv√§
 # RMSEA = 0.046 --> alle 0.05 on ok
 
 # Lataukset kullekin faktorille:
 library(plot.matrix)
 plot(loadings(fa4), cex=0.5, axis.row = list(side=2, las=1))
-# Kaikki muuttujat latautuvat v‰hint‰‰n .3 jollekin faktorille,
-# mutta osalla muuttujista lataukset pieni‰ (alle .4).
+# Kaikki muuttujat latautuvat v√§hint√§√§n .3 jollekin faktorille,
+# mutta osalla muuttujista lataukset pieni√§ (alle .4).
 
 # Muuttujien kommunaliteetit erikseen:
 fa4$communality
@@ -119,21 +119,21 @@ fa4$communality
 # TAS11, TAS14, TAS16, TAS18, TAS19, TAS20
 
 # Faktorien selitysosuus:
-# Yksitt‰isten faktorien selitysosuus 4-12%
+# Yksitt√§isten faktorien selitysosuus 4-12%
 # Kokonaisselitysosuus 31%
 
-# Faktoreiden v‰liset korrelaatiot
+# Faktoreiden v√§liset korrelaatiot
 # kohtalainen (r=0.51) vain faktoreille 1 ja 3
 # vinorotaatio perusteltu
 
-# Faktoreiden nime‰minen
+# Faktoreiden nime√§minen
 # PA1: vaikeus kuvailla tunteita
 # PA2: taipumus ongelmanratkaisuun
-# PA3: vaikeus tunnistaa tunteita tai niiden syit‰ 
+# PA3: vaikeus tunnistaa tunteita tai niiden syit√§ 
 # PA4: haluttomuus jakaa tunteita
 
 # Yhteenvetona:
-# Lataukset ja kommunaliteetit osalle muuttujista pieni‰.
+# Lataukset ja kommunaliteetit osalle muuttujista pieni√§.
 # Kokeillaan kolmen faktorin ratkaisua.
 
 # ---
@@ -160,14 +160,14 @@ fa3
 
 # Faktoriratkaisun sopivuus:
 # khii2(133)=969.08, p<.0001
-# TLI = 0.83 eli ei kovin hyv‰
+# TLI = 0.83 eli ei kovin hyv√§
 # RMSEA = 0.057 eli kohtalainen
 
 # Lataukset kullekin faktorille:
 library(plot.matrix)
 plot(loadings(fa3), cex=0.5, axis.row = list(side=2, las=1))
-# Kaikki muuttujat latautuvat v‰hint‰‰n .3 jollekin faktorille,
-# lataukset parempia kuin nelj‰n faktorin mallissa.
+# Kaikki muuttujat latautuvat v√§hint√§√§n .3 jollekin faktorille,
+# lataukset parempia kuin nelj√§n faktorin mallissa.
 
 # Muuttujien kommunaliteetit erikseen:
 fa3$communality
@@ -176,14 +176,14 @@ fa3$communality
 # TAS11, TAS12, TAS14, TAS15, TAS16, TAS18, TAS19, TAS20
 
 # Faktorien selitysosuus:
-# Yksitt‰isten faktorien selitysosuus 6-12%
+# Yksitt√§isten faktorien selitysosuus 6-12%
 # Kokonaisselitysosuus 28%
 
-# Faktoreiden v‰liset korrelaatiot
+# Faktoreiden v√§liset korrelaatiot
 # kohtalainen (r=0.56) vain faktoreille 1 ja 3
 # vinorotaatio perusteltu
 
-# Faktoreiden nime‰minen
+# Faktoreiden nime√§minen
 # PA1: vaikeus puhua tunteista
 # PA2: vaikeus tunnistaa tunteita
 # PA3: taipumus ulkoisesti suuntautuneeseen ajatteluun
