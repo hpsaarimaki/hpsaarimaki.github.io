a27ca2b46a4bfa8cea2a38720f73566ab086e806
hpsaarimaki <57613974+hpsaarimaki@users.noreply.github.com>
2024-09-02 11:58:04 +0300

    Update Luentoesimerkki-2.2.r

--- files/data/Luentoesimerkki-2.2.r
+++ files/data/Luentoesimerkki-2.2.r
@@ -1,196 +1,150 @@
 # PSY.204
-# HS 1.9.2023
-# Esimerkkiaineisto: Blindspots and spotlights
+# Luento 2.2
+# T-testit
+# Esimerkkiskripti
+# HS 2.9.2024
 
 # ---
 
 # Asetetaan työskentelykansio:
+setwd('C:/Users/sbhesa/OneDrive - TUNI.fi/Opetus/2024-2025/PSY.204 syksy 2024/Harjoitukset')
 
-setwd("C:/Users/sbhesa/Documents/Opetus/2023-2024/PSY204 - syksy 2023/Viikko 2")
+# Aineiston lataaminen ja valmistelu:
+source('Harjoitukset-R1.2.r')
 
-# ---
-
-# 1. Aineiston valmistelu
-
-# Ladataan aineisto
-
-data <- read.csv('https://hpsaarimaki.github.io/files/data/scotoma.csv')
-head(data)
-
-# Huomioi, että aineisto on pitkässä muodossa (eri koetilanteet eri riveillä).
-# Tämä on hyvä, koska tilastollisen testin ajava funktio (aov_car()) edellyttää pitkää muotoa.
-
-# Tarkastellaan aineistoa:
-summary(data)
-
-# Muutetaan muuttuja 'subject' faktoriksi:
-
-data$subject <- as.factor(data$subject)
-
-# Puuttuvat arvot: puuttuvia arvoja ei näytä olevan
-
-# Kiinnitetään tietokehys:
-
-attach(data)
+# Kiinnitetään aineisto:
+attach(naming)
 
 # ---
 
-# 2. Kuvailevat tulokset
+# Aineiston kuvailu
 
-# Keski- ja hajontaluvut:
+# Keski- ja hajontaluvut koko tietokehykselle:
 library(psych)
-describe(data)
+describe(naming)
 
-# Esim. keskiarvot koetilanteittain:
+# Keski- ja hajontaluvut erikseen eri ryhmille:
+tapply(iq, list(sex, reading.group), mean, na.rm=T)	# keskiarvo (mean)
+tapply(iq, list(sex, reading.group), sd, na.rm=T)	# keskihajonta (sd)
 
-mean_by_type = tapply(RT, degradation_type, mean)
-mean_by_type # näkökentän muutoksen tyyppi
 
-mean_by_window = tapply(RT, window_size, mean)
-mean_by_window # näkökentän muutoksen koko
-
-mean_by_both = tapply(RT, list(window_size, degradation_type), mean)
-mean_by_both # näkökentän muutoksen tyyppi ja koko
-
-# Histogrammi:
-hist(RT, main="Reaktioajan histogrammi", ylab="Frekvenssi", xlab="Reaktioaika (ms)")
-
-# Histogrammiesimerkki ggplot2:lla:
-library(ggplot2)
-ggplot(data, aes(x=RT)) + 
-  geom_histogram(aes(y=..density..), colour="black", fill="white")+
-  geom_density(alpha=.2, fill="#FF6666") +
-  labs(x = "Reaktioaika (ms)", y = "Osuus")
-
-# Laatikkokuvio:
-boxplot(RT~window_size, main="Reaktioaika koetilanteittain", ylab="Reaktioaika (ms)", xlab="Ikkunan koko")
-
-# Laatikkokuvioesimerkki ggplot2:lla:
-ggplot(data, aes(x=window_size, y=RT, fill=window_size)) +
-  geom_boxplot() +
-  labs(y = "Reaktioaika (ms)", x = "Koetilanne") + 
-  theme(legend.title=element_blank())
-
-# Numeerinen yhteenveto laatikkokuviosta:
-tapply_table = tapply(RT, window_size, mean)
-tapply_table # näyttää taulukon konsolissa
-
-# Tallennetaan numeerinen yhteenveto tiedostoon:
-write.csv(tapply_table, file="RT_mean_by_window_size.csv") 
 
 # ---
 
-# 3. Mallin rakentaminen
+# Yhden otoksen t-testi
 
-# Luentokalvoissa on käytetty kolmea esimerkkimallia.
-# Tallennetaan mallit tässä, tarkastellaan niitä tarkemmin seuraavissa vaiheissa.
+# Eroaako älykkyys otoksessamme populaation älykkyydestä?
 
-# Esimerkki 1: näkökentän muutoksen koon päävaikutus
-library(afex)
-A1 <- aov_car(RT~Error(subject/window_size), data=data, fun_aggregate=mean)
-# Tässä esimerkissä säädetään argumentti fun_aggregate=mean, koska samalle koetilanteelle on aineistossa useampi rivi per osallistuja.
+# Oletusten tarkastelu:
 
-# Esimerkki 2: näkökentän muutoksen koon ja tyypin pää- ja yhteisvaikutukset:
-A2 <- aov_car(RT~Error(subject/(degradation_type*window_size)), data=data)
+# Jakaumat:
 
-# Esimerkki 3: ryhmän päävaikutus ja näkökentän muutoksen koon ja tyypin pää- ja yhteisvaikutukset:
-# Lisätään aineistoon ensin muuttuja kuvaamaan ryhmää.
-# Tässä esimerkissä ryhmä on täysin satunnainen, joten sillä ei pitäisi olla vaikutusta reaktioaikaan.
-data$group <- rep(1:2, each=84)
-data$group <- factor(data$group)
-A3 <- aov_car(RT~group + Error(subject/(degradation_type*window_size)), data=data)
+hist(iq)              # histogrammi
+qqnorm(iq)            # qq-kuvio
+qqline(iq, col="red")
 
-# -----
+# Normaalijakautuneisuustestit:
 
-# 5. Oletusten testaaminen
+# Shapiro-Wilk
+shapiro.test(iq)	
 
-# Vierashavainnot:
+# Kolmogorov-Smirnov
+ks.test(iq, pnorm, mean=mean(iq, na.rm=T), sd=sd(iq, na.rm=T))
 
-# Tarkastellaan laatikkokuviota:
-boxplot(RT~window_size*degradation_type, main="Reaktioaika koetilanteittain", ylab="Reaktioaika (ms)", xlab="Ikkunan koko")
+# -
 
-# Tarkastellaan laatikkokuvion tuloksia numeerisesti:
-library(rstatix)
-# Koko aineistolle:
-data %>%
-  identify_outliers(RT)
-# Koetilanteittan:
-data %>%
-  group_by(window_size, degradation_type) %>%
-  identify_outliers("RT")
+# Yhden otoksen t-testi:
+t.test(iq, mu=100, data=naming)
 
-# Aineistossa ei ole merkittävästi poikkeavia havaintoja.
+# Efektikoko Cohenin D:
+library(lsr)
+cohensD(naming$iq, mu=100)
 
-# Residuaalien normaalijakautuneisuus, esimerkkinä malli 1:
-library(performance)
-A1_is_norm <- check_normality(A1)
-A1_is_norm # näyttää arvion residuaalien normaalijakautuneisuudesta
+# ---
 
-# Vertailu normaalijakaumaan:
-plot(A1_is_norm) # jakauman muoto
-plot(A1_is_norm, type = "qq") ## qq-kuvio
-plot(A1_is_norm, type = "qq", detrend = TRUE) # qq-kuvio projisoituna
+# Riippumattomien otosten t-testi:
 
-# Residuaalien normaalijakautuneisuus mallille 2:
-A2_is_norm <- check_normality(A2)
-A2_is_norm
+# Oletusten tarkastelu:
 
-# Residuaalien normaalijakautuneisuus mallille 3:
-A3_is_norm <- check_normality(A3)
-A3_is_norm
+# Histogrammit erikseen miehille ja naisille:
+par(mfrow=c(1,2))
+hist(regular, main='Miehet', xlab='Tavalliset sanat (ms)',
+     data=naming_male)
+hist(regular, main='Naiset', xlab='Tavalliset sanat (ms)',
+     data=naming_female)
 
-# Residuaalit ovat normaalijakautuneita kaikissa malleissa.
+# Shapiro-Wilk
+shapiro.test(naming_male$regular)
+shapiro.test(naming_female$regular)
+
+# Varianssien yhtäsuuruus
+var.test(regular~sex, data=naming)
+
+# -
+
+# Riippumattomien otosten t-testi:
+t.test(regular ~ sex, data=naming)
+
+# Efektikoko:
+library(lsr)
+cohensD(regular ~ sex, data=naming)
+
+# Keskiluvut ryhmittäin:
+tapply(iq, list(sex, reading.group), mean, na.rm=T)	# keskiarvo (mean)
+tapply(iq, list(sex, reading.group), sd, na.rm=T)	# keskihajonta (sd)
+
+# Kuvio:
+library(dplyr)
+library(ggplot2)
+naming %>%
+  ggplot(aes(x = sex, y = regular, fill = sex)) +
+  geom_boxplot() +
+  theme(
+    legend.position = "none",
+    plot.title = element_text(size = 11)
+  ) +
+  xlab("") +
+  ylab("Tavalliset sanat (ms)") +  
+  scale_x_discrete(labels = c("female" = "Naiset", "male" = "Miehet"))
+
+
+# -
+
+# Toistettujen mittausten t-testi
+
+# Oletusten testaaminen:
+
+# Histogrammit eri koetilanteille:
+par(mfrow=c(1,2))
+hist(regular, main="", xlab='Tavalliset sanat (ms)', ylab="")
+hist(exception, main="", xlab='Epätavalliset sanat, (ms)', ylab="")
+
+# Shapiro-Wilk eri koetilanteille:
+shapiro.test(regular)
+shapiro.test(exception)
 
 # Varianssien yhtäsuuruus:
-check_sphericity(A1) 
-check_sphericity(A2)
-check_sphericity(A3)
+var.test(regular, exception)
 
-# Varianssit eivät ole yhtäsuuria missään mallissa.
-# Käytetään sfäärisyyskorjauksia ANOVAn yhteydessä (ks. seuraava vaihe).
+# -
 
-# ---
+# Toistettujen mittausten t-testi
+t.test(regular, exception, paired=T, data=naming)
 
-# 5. Tilastollisen testin tulosten tarkastelu
+# Efektikoko
+cohensD(regular, exception, method="paired")
 
-# Malli 1:
-summary(A1)
-# Näytön muutoksen koko vaikuttaa reaktioaikaan (F(2,54)=65.45, p[GG]<.001)
-
-# Malli 2:
-summary(A2)
-# Näytön muutoksen koon ja tyypin yhteisvaikutus reaktioaikaan on tilastollisesti merkitsevä (F2,54)=44.71, p<.001).
-# Myös näytön muutoksen tyypin päävaikutus on merkitsevä (F(1,27)=330.93, p<.001).
-# Kuten myös näytön muutoksen koon päivaikutus (F(2,54)=65.60, p[GG]<.001).
-
-# Malli 3:
-summary(A3)
-# Ryhmän vaikutus ei ole tilastollisesti merkitsevä eikä sillä ole myöskään merkitsevää yhteisvaikutusta koetilanteiden kanssa.
-# Muut tulokset ovat samat kuin yllä kuvatut.
-
-# ---
-
-# 6. Raportointi
-
-# Interaktiokuvaaja mallille 2:
-
-detach(data)
-attach(data)
-interaction.plot(window_size, degradation_type, RT,
-                 type="b", col=c(1:2),
-                 leg.bty="o",leg.bg="beige",
-                 lwd=2, pch=c(18,18),
-                 xlab="Window size",
-                 ylab="Reaction time (ms)",
-                 main="Interaction plot")
-
-# Numeerinen yhteenveto interaktiokuvaajasta:
-
-tapply_table = tapply(RT, list(degradation_type, window_size), mean)
-tapply_table
-
-write.csv(tapply_table, file="RT_mean_by_condition.csv") # tallennetaan tiedostoon
-
+# Tulosten visualisointi laatikkokuvion avulla:
+ggplot() +
+  geom_boxplot(aes(x = "Tavalliset sanat", y = naming$regular), fill = "aquamarine") +
+  geom_boxplot(aes(x = "Epätavalliset", y = naming$exception), fill = "chocolate") +
+  theme(
+    legend.position = "none",
+    plot.title = element_text(size = 11)
+  ) +
+  xlab("Sanan tyyppi") +
+  ylab("Lukemiseen käytetty aika (ms)") +
+  scale_x_discrete(labels = c("Tavallinen", "Epätavallinen"))
 
 # ---
 
